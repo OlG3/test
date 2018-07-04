b9781d8974b1e9a62d50d2089b7d594006c73bfb
geronimo <o.supruniuk@internetdevels.com>
2018-07-04 17:22:24 +0300

    Some task

--- code.php
+++ code.php
@@ -5,22 +5,58 @@
 //
 //echo '<pre>';
 
-try {
 
-    if(/*empty($_POST['numb']) == false &&*/ is_numeric($_POST['numb'])) {
+//Введене число більше чи менше нуля
+//    if(/*empty($_POST['numb']) == false &&*/ is_numeric($_POST['numb'])) {
+//
+//        $numb = $_POST['numb'];
+//
+//        if ($numb > 0) {
+//            echo('The number more than 0');
+//        } else if ($numb < 0) {
+//            echo('The number less than 0');
+//        } else if ($numb == 0) {
+//            echo('The number equal zero');
+//        } else {
+//            echo('This is not a number');
+//
+//        }
+//    }
 
-        $numb = $_POST['numb'];
+// рахує кількість слів в реченні. Не працює для кирилиці.
+//$var = $_POST['numb'];
+//
+//$counts = str_word_count($var);
+//
+//echo($counts);
+//echo "<pre>";
+//print_r($_POST);
+//echo "</pre>";
+//die('1');
+//$var1 = $_POST['first_numb'];
 
-        if ($numb > 0) {
-            echo('The number more than 0');
-        } else if ($numb < 0) {
-            echo('The number less than 0');
-        } else if ($numb == 0) {
-            echo('The number equal zero');
-        } else {
-            throw new Exception('Єто не число');
+
+// рахує кількість парних чисел в проміжку між введеними числами
+$result = [];
+
+if(!empty($_POST['submit_diget'] == 'Submit')){ // перевірка на встановлення значення
+
+    for ($i = $_POST['first_numb']+1; $i < $_POST['second_numb']; $i++){
+        if($i % 2 == 0){
+            $result[] = $i;
         }
     }
-}catch (Exception $e){
-    echo 'No datainfo', $e->getMessage();
+    $count = count($result);
 }
+echo $count;
+
+
+
+//echo "<pre>";
+//print_r($result);
+//echo "</pre>";
+
+
+
+
+
