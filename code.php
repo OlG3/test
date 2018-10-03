8a9e614fe52fac15ccb64ab7f39e87388c174020
geronimo <o.supruniuk@internetdevels.com>
2018-10-03 14:19:53 +0300

    New connection to DB and queries

--- code.php
+++ code.php
@@ -1,42 +1,66 @@
 <?php
-require_once 'database.php';
+//require_once 'database.php';
+/*
+//set connection to bd
+$con = mysqli_connect('localhost', 'root', 'root', 'learn');
 
+//Set russian coding
+mysqli_set_charset($con, 'utf8');
 
-$table = '';
-
-$sql = "SELECT * FROM `workers` WHERE `id` != 2";
-
-$stm = $pdo -> prepare( $sql );
-
-//echo '<pre>';
-//print_r( $stm );
-//echo '</pre>';
-
-//die('1');
-
-$stm -> execute(  );
-
-$all = $stm -> fetchAll(  );
-
-foreach ($all as $row){
-  $table .= "<tr>";
-  $table .= "<td>".$row['id']."</td>";
-  $table .= "<td>".$row['name']."</td>";
-  $table .= "<td>".$row['age']."</td>";
-  $table .= "<td>".$row['salary']."</td>";
-
-  $table .= "</tr>";
+// Check connection
+if (mysqli_connect_errno()) {
+  echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
 }
-//echo '<pre>';
-//print_r( $all );
-//echo '</pre>';
+
+// query
+//$query = "SELECT * FROM user";
+$query = "SELECT * FROM user WHERE `name`='Сергей' AND `age`='66'";
+
+// query result
+$result = mysqli_query($con, $query);
+
+// how many rows returned
+$count = mysqli_num_rows($result);
+
+//my arr
+$myArr = [];
+
+if($count) {
+  $index = 0;
+  while ($row = mysqli_fetch_array($result)) {
+    echo '<h1>';
+    echo $row['id'];
+    echo '</h1>';
+  }
+}
+*/
+
+///////////////////////////////////////////////////////////
+
+//set connection to bd
+$con = mysqli_connect('localhost', 'root', 'root', 'learn');
+
+//Set russian coding
+mysqli_set_charset($con, 'utf8');
+
+// Check connection
+if(mysqli_connect_errno()) {
+  echo 'Failed to connect to MySQL: ' . mysqli_connect_error();
+}
+
+// query
+//$query = "INSERT INTO news VALUES (NULL, 'Some header', 'abc content', 'full content', '2', '2', CURRENT_TIMESTAMP , '2', '1', '1');";
+//$query = "INSERT INT news_category VALUES (NULL, 'value111','desc111',1,1);";
+//$query = "INSERT INTO news_category(name, description) VALUES('name1','desc1'), ('name2','desc2');";
+$query = "DELETE FROM user WHERE `id`=16;";
 
 
 
+// send and return result of query
+$info = mysqli_query($con, $query);
 
-
-?>
-
+// state of result(true or false)
+echo 'Result of INSERT: '.(boolval($info) ? 'true' : 'false');
 
 
 
